7a953f226949505ea2d08760e8cc4e86092c390e
SeongUk Cho <tjddnr377@naver.com>
2025-05-23 17:31:55 +0900

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes, Link, Outlet, useNavigate } from "react-router-dom";
+import List from "./event/List";
+import Regist from "./event/Regist";
+import Detail from "./event/Detail";
+import Login from "./user/Login";
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Layout() {
+    const [isLogin, setIsLogin] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleLogout = () => {
+        window.sessionStorage.removeItem("access_token");
+        setIsLogin(false);
+    };
+
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const token = window.sessionStorage.getItem("access_token");
+        if (token) {
+            setIsLogin(true);
+        } else {
+            setIsLogin(false);
+        }
+    });
+
+    useEffect(() => {
+        if(isLogin) {
+            navigate("/list");
+        } else {
+            navigate("/login");
+        }
+    }, [isLogin]);
+
+    return (
+        <>
+            <h1>이벤트 관리 시스템</h1>
+            <hr />
+            <header>
+                {
+                    isLogin ? (
+                        <a onClick={handleLogout}>로그아웃</a>
+                    ) : (
+                        <Link to="/login">로그인</Link>
+                    )
+                }
+            </header>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+                <p>이벤트 관리 시스템 © 2023</p>
+            </footer>
+        </>
+    );
 }
 
-export default App
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/regist" element={<Regist />} />
+                        <Route path="/list" element={<List />} />
+                        <Route path="/detail/:event_id" element={<Detail />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+export default App;
